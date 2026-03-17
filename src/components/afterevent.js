de0d13ebf56125d278f2a4f8bffde8d0175d438b
iysaofficial <youngscientist.iysa@gmail.com>
2026-03-17 09:41:11 +0700

    UP AFTEREVENT

--- src/components/afterevent.js
+++ src/components/afterevent.js
@@ -6,7 +6,7 @@ const AfterEvent = () => {
     <section className="afterevent-section">
       <div className="container text-center mb-5">
         <h1>AFTER EVENT LKTIN 2026</h1>
-        <iframe width="100%" height="560" src="https://www.youtube.com/embed/pVDThogh0xQ?si=oI3HixDm14JDsP2s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="100%" height="560" src="https://www.youtube.com/embed/K94L31bRuz4?si=aOn47hrW6fWDz4KG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </section>
   );
