f1f7d0d52a6aba8d323cf0b02773321590ff9ed5
iysaofficial <youngscientist.iysa@gmail.com>
2026-05-07 15:06:20 +0700

    UP SK 2025

--- src/pages/low/2025/jenjang25.js
+++ src/pages/low/2025/jenjang25.js
@@ -6,30 +6,44 @@ function Jenjang25() {
     <section className="low-section">
       <div className="her mt-5">
         <div id="low" class="cards mx-auto text-center">
-          <h1 class="card-header text-center p-3">Daftar Pemenang 2025</h1>
+          <h1 class="card-header text-center p-2">Daftar Pemenang 2025</h1>
           <h3>Pilih berdasarkan jenjang event yang kalian ikuti</h3>
           <div className="wrapper-btn text-center">
             <a
               className="btn slep"
-              href="https://drive.google.com/file/d/1aI1DXyUdovFfcfDaFO2QHGV0jmrndtjn/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1WjmRHjYhUtymq2PLtVMIvfj1Y6a494yY/view?usp=sharing"
               target="_blank" rel="noreferrer"
             >
               Spesial Award
             </a>
-            {/* <a
+            <a
               className="btn slep"
-              href="https://drive.google.com/file/d/1is6djM5dXLTgGaz9bXtd-am9JcUevLLl/view?usp=sharing"
+              href="https://drive.google.com/file/d/1H7LCMVxVccrTpAPUeSHZtZilv4UohHtm/view?usp=sharing"
               target="_blank" rel="noreferrer"
             >
-              Sekolah Menengah Atas (SMA)
+              Universitas
             </a>
             <a
               className="btn slep"
-              href="https://drive.google.com/file/d/1UqXS0gG09yiqLAJ8iec0WMY0B4wZIfsU/view?usp=sharing"
+              href="https://drive.google.com/file/d/1l_Fhb8a1p8D0ImVAhYmwq15V332sFPFk/view?usp=sharing"
               target="_blank" rel="noreferrer"
             >
-              Sekolah Menengah Pertama (SMP)
-            </a> */}
+              Sekolah Menengah Atas
+            </a>
+            <a
+              className="btn slep"
+              href="https://drive.google.com/file/d/1wkZ8W5fUbmtA7Tnk5tW-qt7yt-gAyk2t/view?usp=sharing"
+              target="_blank" rel="noreferrer"
+            >
+              Sekolah Menengah Pertama
+            </a> 
+            <a
+              className="btn slep"
+              href="https://drive.google.com/file/d/1STvZN_tFtC3OSCKDf4EyDdjK62zTNhPJ/view?usp=sharing"
+              target="_blank" rel="noreferrer"
+            >
+              Sekolah Dasar
+            </a> 
           </div>
         </div>
       </div>
